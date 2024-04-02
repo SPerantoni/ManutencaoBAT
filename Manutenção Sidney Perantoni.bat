b11a25386dc6d0963e7197dd9218a15563dc934d
Sidney PErantoni <165850636+SPerantoni@users.noreply.github.com>
2024-04-02 19:48:50 -0300

    Add files via upload
    Na minha maquina as vezes o CleanMGR trava, se passou muito tempo eu abro o gerenciador de tarefas (para abrir o gerenciador precione CTRL + SHIFT + ESC), botaão direito nele na guia detalhes e finalizo a arvore
    ** Já aconteceu do SFC ficar travado tambem, cliquei no prompt (esta tela) e dei enter algumas vezes e ele voltou ao normal
    *** Não me responsabilizo por nada, leia o codigo e use se quiser, sabendo o que pode ou nao pode na sua maquina
    **** O processo é demorado, na minha maquina o processo todo dura cerca de 13h, logo se notebook, mantenha conectado a fonte de energia

--- Manutenção Sidney Perantoni.bat
+++ Manutenção Sidney Perantoni.bat
@@ -8,7 +8,7 @@ cls
 Echo .
 Echo .
 Echo As palavras estao sem acento ou caracteres especiais, pois aqui e dificil de colocar...
-Echo * Na minha maquina as vezes o CleanMGR trava, se passou muito tempo eu abro o gerenciador de tarefas, botaão direito nele na guia detalhes e finalizo a arvore
+Echo * Na minha maquina as vezes o CleanMGR trava, se passou muito tempo eu abro o gerenciador de tarefas (para abrir o gerenciador precione CTRL + SHIFT + ESC), botaão direito nele na guia detalhes e finalizo a arvore
 Echo ** Já aconteceu do SFC ficar travado tambem, cliquei no prompt (esta tela) e dei enter algumas vezes e ele voltou ao normal
 Echo *** Não me responsabilizo por nada, leia o codigo e use se quiser, sabendo o que pode ou nao pode na sua maquina
 Echo **** O processo é demorado, na minha maquina o processo todo dura cerca de 13h, logo se notebook, mantenha conectado a fonte de energia
