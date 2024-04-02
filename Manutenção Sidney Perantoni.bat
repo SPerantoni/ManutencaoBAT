05008c7650f41b6884ae8fbf712ecd81674b5214
Sidney PErantoni <165850636+SPerantoni@users.noreply.github.com>
2024-04-02 20:22:50 -0300

    Manutenção Sidney Perantoni.bat

--- Manutenção Sidney Perantoni.bat
+++ Manutenção Sidney Perantoni.bat
@@ -12,11 +12,12 @@ Echo * Na minha maquina as vezes o CleanMGR trava, se passou muito tempo eu abro
 Echo ** Já aconteceu do SFC ficar travado tambem, cliquei no prompt (esta tela) e dei enter algumas vezes e ele voltou ao normal
 Echo *** Não me responsabilizo por nada, leia o codigo e use se quiser, sabendo o que pode ou nao pode na sua maquina
 Echo **** O processo é demorado, na minha maquina o processo todo dura cerca de 13h, logo se notebook, mantenha conectado a fonte de energia
+***** Se quiser parar o prompt é só apertar CTRL + C, para voltar a parte grafica usa o gerenciador de tarefas (para abrir o gerenciador precione CTRL + SHIFT + ESC), Arquivo, Executar nova tarefa, vai abrir uma caixa (igual o win+r), escreve Explorer.exe e da enter.
 Echo .
 Echo .
 Echo Voce esta utilizando um SSD como Armazenamento?
 Echo. ---------------------------
-Echo Abaixo nome, se estiver "Fixed hard disk media" se trata de um HDD (Usar opcao 1, se nao quiser checkdisk use opcao 2), ja se estiver, "Solid state media" é um SSD, use opcao 2 pois não pode fazer checkdisk em SSD (Queima os ciclos):
+Echo Abaixo nome, se estiver "Fixed hard disk media" se trata de um HDD (Usar opcao 1, se nao quiser desfragmentar use opcao 2), ja se estiver, "Solid state media" é um SSD, use opcao 2 pois não pode desfragmentar em SSD (Queima os ciclos):
 Echo .
 Echo .
 wmic diskdrive get mediatype
